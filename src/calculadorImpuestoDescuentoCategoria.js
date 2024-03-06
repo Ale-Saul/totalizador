92bbbcc38cff2bae370362b6613446ae2e1143ee
Andres Revollo <andres.revollo@usb.edu.bo>
2024-03-06 00:35:40 -0400

    Se añadio el impuesto para la categoria Muebles

--- src/calculadorImpuestoDescuentoCategoria.js
+++ src/calculadorImpuestoDescuentoCategoria.js
@@ -16,6 +16,10 @@ function calcularImpuestoDescuentoCategoria(neto, categoria){
         descuento = neto * 1.50 / 100;
         return impuesto - descuento;
     }
+    else if (categoria == "Muebles"){
+        impuesto = neto * 3.00 / 100;
+        return impuesto - descuento;
+    }
     else {
         return 0;
     }
