9ab2c6681ff33779429c9abfd5533322661465a0
Alejandro <alejandro.mont.cesp@gmail.com>
2024-03-05 22:07:06 -0400

    Se refactorizo los metodos para calcular descuetos e impuestos

--- src/calculador.js
+++ src/calculador.js
@@ -1,36 +1,36 @@
 function calcular(precio, cantidad, estado) {
   var neto = precio * cantidad;
-  neto = calcularImpuestoPorEstado(neto, estado);
-  neto = calcularDescuentoPorCantidad(neto, cantidad);
-  return neto;
+  var impuesto = calcularImpuestoPorEstado(neto, estado);
+  var descuento = calcularDescuentoPorCantidad(neto, cantidad);
+  return neto + impuesto - descuento;
 }
 function calcularImpuestoPorEstado(neto, estado){
 
   if (estado == "CA") {
-    return neto + neto * 8.25 / 100;
+    return neto * 8.25 / 100;
   } 
   else if(estado =="UT"){
-    return neto + neto * 6.65 / 100;
+    return neto * 6.65 / 100;
   }
   else if(estado =="NV"){
-    return neto + neto * 8.00 / 100;
+    return neto * 8.00 / 100;
   }
   else if(estado =="TX"){
-    return neto + neto * 6.25 / 100;
+    return neto * 6.25 / 100;
   }
   else if(estado =="AL"){
-    return neto + neto * 4.00 / 100;
+    return neto * 4.00 / 100;
   }
   else{
-    return neto;
+    return 0;
   }
 }
 function calcularDescuentoPorCantidad(neto,cantidad){
   if (cantidad == 1000) {
-    return neto - neto * 3.00 / 100;
+    return neto * 3.00 / 100;
   }  
   else {
-    return neto;
+    return 0;
   }
 }
 
