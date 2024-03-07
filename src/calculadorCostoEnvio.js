1cca8e5aa93988e1355374d9ae8ed1b97f474c27
Andres Revollo <andres.revollo@usb.edu.bo>
2024-03-06 21:13:32 -0400

    Se añadio el costo de envio para un peso entre 11 y 20

--- src/calculadorCostoEnvio.js
+++ src/calculadorCostoEnvio.js
@@ -2,6 +2,9 @@ function calculadorCostoEnvio(peso) {
     if(peso > 0 && peso <= 10){
         return 0;
     }
+    else if (peso > 10 && peso <= 20){
+        return 3.5;
+    }
 }
 
 export default calculadorCostoEnvio;
