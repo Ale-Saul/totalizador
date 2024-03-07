b9c0cc0e68937464dc25dd948272d7702b4a335b
Andres Revollo <andres.revollo@usb.edu.bo>
2024-03-06 22:01:38 -0400

    Se añadio el decuento para el tipo de cliente recurrente

--- src/calculadorDecuentoEnvio.spec.js
+++ src/calculadorDecuentoEnvio.spec.js
@@ -4,4 +4,7 @@ describe("Calculador de descuento de envío", () => {
     it("deberia calcular el descuento de envío de un cliente normal", () => {
         expect(calcularDescuentoEnvio(100, "normal")).toEqual(0);
     });
+    it("deberia calcular el descuento de envío de un cliente recurrente", () => {
+        expect(calcularDescuentoEnvio(100, "recurrente")).toEqual(0.5);
+    });
 });
